c235c4daede7a279f080cfb731476f02e910d32b
zhanghowe <zhanghowe122@gmail.com>
2024-12-23 16:09:38 +0800

    1. 线程安全的队列模版的实现

--- RemoteCtrl/RemoteCtrl/MyQueue.h
+++ RemoteCtrl/RemoteCtrl/MyQueue.h
@@ -1,49 +1,199 @@
 #pragma once
 #include <list>
+#include <mutex>
+#include "pch.h"
 // 线程安全的队列，利用IOCP实现
 template<class T>
 class CMyQueue
 {
 public:
-	CMyQueue();
-	~CMyQueue();
-	bool PushBack(const T&data);
-	bool PopFront(const T& data);
-	size_t Size();
-	void Clear();
-
-private:
-	static void threadEntry(void* arg);
-	void threadMain();
-
-private:
-	std::list<T> m_lstData;
-	HANDLE m_hCompeletionPort;
-	HANDLE m_hThread;
-
-public:
-	typedef struct IocpParam {
-		int nOperator; // 操作
-		T strData; // 数据
-		HANDLE hEvent; //pop操作需要的
-
-		IocpParam(int op, const char* sData, _beginthread_proc_type cb = NULL) {
-			nOperator = op;
-			strData = sData;
-		}
-
-		IocpParam() {
-			nOperator = -1;
-		}
-
-	}PPARAM; // post Parameter 用于投递信息的结构体
-
 	enum {
+		MQNone,
 		MQPush,
 		MQPop,
 		MQSize,
 		MQClear
 	};
+
+	typedef struct IocpParam {
+		size_t nOperator; // 操作
+		T Data; // 数据
+		HANDLE hEvent; //pop操作需要的
+
+		IocpParam(int op, const T& data, HANDLE hEve = NULL) {
+			nOperator = op;
+			Data = data;
+			hEvent = hEve;
+		}
+
+		IocpParam() {
+			nOperator = MQNone;
+		}
+
+	}PPARAM; // post Parameter 用于投递信息的结构体
+
+public:
+	CMyQueue() {
+		m_lock = false;
+		m_hCompeletionPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 1);
+		m_hThread = INVALID_HANDLE_VALUE;
+		if (m_hCompeletionPort != NULL) {
+			m_hThread = (HANDLE)_beginthread(
+				&CMyQueue<T>::threadEntry,
+				0, m_hCompeletionPort);
+		}
+	}
+
+	~CMyQueue() {
+		if (m_lock) return;
+		m_lock = true;
+		HANDLE hTemp = m_hCompeletionPort;
+		PostQueuedCompletionStatus(m_hCompeletionPort, 0, NULL, NULL);
+		WaitForSingleObject(m_hThread, INFINITE);
+		m_hCompeletionPort = NULL;
+		CloseHandle(hTemp);
+	}
+
+	bool PushBack(const T& data) {
+		IocpParam* pParam = new IocpParam(MQPush, data);
+		if (m_lock) {
+			delete pParam;
+			return false;
+		}
+		bool ret = PostQueuedCompletionStatus(m_hCompeletionPort, sizeof(PPARAM), (ULONG_PTR)pParam, NULL);
+		if (ret == false) delete pParam;
+		return ret;
+	}
+
+	bool PopFront(T& data) {
+		HANDLE hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+		IocpParam Param(MQPop, data, hEvent);
+		if (m_lock) {
+			if (hEvent) CloseHandle(hEvent);
+			return false;
+		}
+		bool ret = PostQueuedCompletionStatus(m_hCompeletionPort, sizeof(PPARAM),
+			(ULONG_PTR)&Param, NULL);
+		if (ret == false) {
+			CloseHandle(hEvent);
+			return false;
+		}
+		ret = WaitForSingleObject(hEvent, INFINITE) == WAIT_OBJECT_0;
+		if (ret) {
+			data = Param.Data;
+		}
+		return ret;
+	}
+
+	size_t Size() {
+		HANDLE hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+		IocpParam Param(MQSize, T(), hEvent);
+
+		if (m_lock) {
+			if (hEvent) CloseHandle(hEvent);
+			return -1;
+		}
+		bool ret = PostQueuedCompletionStatus(m_hCompeletionPort, sizeof(PPARAM),
+			(ULONG_PTR)&Param, NULL);
+		if (ret == false) {
+			CloseHandle(hEvent);
+			return -1;
+		}
+		ret = WaitForSingleObject(hEvent, INFINITE) == WAIT_OBJECT_0;
+		if (ret) {
+			return Param.nOperator;
+		}
+		return -1;
+	}
+
+	bool Clear() {
+		if (m_lock) return false;
+		IocpParam* pParam = new IocpParam(MQClear, T());
+		bool ret = PostQueuedCompletionStatus(m_hCompeletionPort, sizeof(PPARAM),
+			(ULONG_PTR)pParam, NULL);
+		if (ret == false) delete pParam;
+		return ret;
+	}
+
+private:
+	static void threadEntry(void* arg) {
+		CMyQueue<T>* thiz = (CMyQueue<T>*)arg;
+		thiz->threadMain();
+		_endthread();
+	}
+
+	void DealParam(PPARAM* pParam) {
+		switch (pParam->nOperator)
+		{
+		case MQPush:
+			m_lstData.push_back(pParam->Data);
+			delete pParam;
+			break;
+
+		case MQPop:
+		{
+			if (m_lstData.size() > 0) {
+				pParam->Data = m_lstData.front();
+				m_lstData.pop_front();
+			}
+			if (pParam->hEvent != NULL)
+				SetEvent(pParam->hEvent);
+		}
+		break;
+
+		case MQSize:
+			pParam->nOperator = m_lstData.size();
+			if (pParam->hEvent != NULL)
+				SetEvent(pParam->hEvent);
+			break;
+
+		case MQClear:
+			m_lstData.clear();
+			delete pParam;
+			break;
+
+		default:
+			OutputDebugStringA("Unknown operator!\r\n");
+			break;
+		}
+	}
+
+	void threadMain() {
+		DWORD dwTransferred = 0;
+		PPARAM* pParam = NULL;
+		ULONG_PTR completionKey = 0;
+		OVERLAPPED* pOverlAppend = NULL;
+		while (GetQueuedCompletionStatus(m_hCompeletionPort,
+			&dwTransferred,
+			&completionKey,
+			&pOverlAppend, INFINITE)) {
+			if ((dwTransferred == 0) || (completionKey == NULL)) {
+				printf("Thread is prepare to exit!\r\n");
+				break;
+			}
+			pParam = (PPARAM*)completionKey;
+			DealParam(pParam);
+		}
+
+		while (GetQueuedCompletionStatus(m_hCompeletionPort,
+			&dwTransferred,
+			&completionKey,
+			&pOverlAppend, 0)) {
+			if ((dwTransferred == 0) || (completionKey == NULL)) {
+				printf("Thread is prepare to exit!\r\n");
+				continue;
+			}
+			pParam = (PPARAM*)completionKey;
+			DealParam(pParam);
+		}
+
+		CloseHandle(m_hCompeletionPort);
+	}
+
+private:
+	std::list<T> m_lstData;
+	HANDLE m_hCompeletionPort;
+	HANDLE m_hThread;
+	std::atomic<bool> m_lock; // 队列正在析构
+
 };
-
-
--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -7,6 +7,7 @@
 #include "Command.h"
 #include "CommonTool.h"
 #include <conio.h>
+#include "MyQueue.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -133,47 +134,28 @@ int main()
 
 	printf("press any key to exit ...\r\n");
 
-	HANDLE hIOCP = INVALID_HANDLE_VALUE; // IO Completion Port
-
-	hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 1); // epoll的区别点1
-
-	if (hIOCP == INVALID_HANDLE_VALUE || hIOCP == NULL) {
-		printf("Create iocp failed!%d\r\n", GetLastError());
-		return 1;
-	}
-
-	HANDLE hThread = (HANDLE)_beginthread(threadQueueEntry, 0, hIOCP);
-
-	ULONGLONG tick = GetTickCount64();
-	ULONGLONG tick0 = GetTickCount64();
-	int count = 0, count0 = 0;
+	CMyQueue<std::string> lstStrings;
+	ULONGLONG tick0 = GetTickCount64(), tick = GetTickCount64();
 
 	while (_kbhit() == 0) { // 完成端口 把请求与实现分离开了
-		if (GetTickCount64() - tick > 1300) {
-			PostQueuedCompletionStatus(hIOCP, sizeof(IOCP_PARAM), (ULONG_PTR)new IOCP_PARAM(IocpListPop, "hello world", func), NULL);
-			tick = GetTickCount64();
-			count++;
+		if (GetTickCount64() - tick0 > 1300) {
+			lstStrings.PushBack("hello world");
+			tick0 = GetTickCount64();
 		}
 
-		if (GetTickCount64() - tick0 > 2000) {
-			PostQueuedCompletionStatus(hIOCP, sizeof(IOCP_PARAM), (ULONG_PTR)new IOCP_PARAM(IocpListPush, "hello world"), NULL);
-			tick0 = GetTickCount64();
-			count0++;
+		if (GetTickCount64() - tick > 2000) {
+			std::string str;
+			lstStrings.PopFront(str);
+			tick = GetTickCount64();
+			printf("Pop from queue:%d\r\n", str.c_str());
 		}
 
 		Sleep(1);
 	}
 
-	if (hIOCP != NULL) {
-		PostQueuedCompletionStatus(hIOCP, 0, NULL, NULL);
-		WaitForSingleObject(hThread, INFINITE);
-	}
-
-	CloseHandle(hIOCP);
-
-	printf("exit done! count = %d count0 = %d\r\n", count, count0);
-
-	printf("exit done!\r\n");
+	printf("exit done! size = %d \r\n", lstStrings.Size());
+	lstStrings.Clear();
+	printf("Clear done! size = %d \r\n", lstStrings.Size());
 	::exit(0);
 
 	/*
